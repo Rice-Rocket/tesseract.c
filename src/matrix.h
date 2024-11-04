abc589081f012497675369ca5e0a1407d150289e
Killian Hourihan <kllnhrhn@gmail.com>
2024-11-03 18:15:41 -0800

    Add rotation matrix macros

--- src/matrix.h
+++ src/matrix.h
@@ -21,6 +21,81 @@ typedef struct {
     float m[2];
 } Vec2;
 
+#define R3x(cos, sin)                                                          \
+    Mat3 {                                                                     \
+        .m = { {1, 0, 0}, {0, cos, -sin}, {0, sin, cos} }                      \
+    }
+
+#define R3y(cos, sin)                                                          \
+    Mat3 {                                                                     \
+        .m = { {cos, 0, -sin}, {0, 1, 0}, {sin, 0, cos} }                      \
+    }
+
+#define R3z(cos, sin)                                                          \
+    Mat3 {                                                                     \
+        .m = { {cos, -sin, 0}, {sin, cos, 0}, {0, 0, 1} }                      \
+    }
+
+#define R4xy(cos, sin)                                                         \
+    Mat4 {                                                                     \
+        .m = {                                                                 \
+            {cos, -sin, 0, 0},                                                 \
+            {sin, cos, 0, 0},                                                  \
+            {0, 0, 1, 0},                                                      \
+            {0, 0, 0, 1}                                                       \
+        }                                                                      \
+    }
+
+#define R4xz(cos, sin)                                                         \
+    Mat4 {                                                                     \
+        .m = {                                                                 \
+            {cos, 0, -sin, 0},                                                 \
+            {0, 1, 0, 0},                                                      \
+            {sin, 0, cos, 0},                                                  \
+            {0, 0, 0, 1}                                                       \
+        }                                                                      \
+    }
+
+#define R4xw(cos, sin)                                                         \
+    Mat4 {                                                                     \
+        .m = {                                                                 \
+            {cos, 0, 0, -sin},                                                 \
+            {0, 1, 0, 0},                                                      \
+            {0, 0, 1, 0},                                                      \
+            {sin, 0, 0, cos}                                                   \
+        }                                                                      \
+    }
+
+#define R4yz(cos, sin)                                                         \
+    Mat4 {                                                                     \
+        .m = {                                                                 \
+            {1, 0, 0, 0},                                                      \
+            {0, cos, -sin, 0},                                                 \
+            {0, sin, cos, 0},                                                  \
+            {0, 0, 0, 1}                                                       \
+        }                                                                      \
+    }
+
+#define R4yw(cos, sin)                                                         \
+    Mat4 {                                                                     \
+        .m = {                                                                 \
+            {1, 0, 0, 0},                                                      \
+            {0, cos, 0, -sin},                                                 \
+            {0, 0, 1, 0},                                                      \
+            {0, sin, 0, cos}                                                   \
+        }                                                                      \
+    }
+
+#define R4zw(cos, sin)                                                         \
+    Mat4 {                                                                     \
+        .m = {                                                                 \
+            {1, 0, 0, 0},                                                      \
+            {0, 1, 0, 0},                                                      \
+            {0, 0, cos, -sin},                                                 \
+            {0, 0, sin, cos}                                                   \
+        }                                                                      \
+    }
+
 Mat3 matmul33(Mat3 *a, Mat3 *b);
 Mat4 matmul44(Mat4 *a, Mat4 *b);
 

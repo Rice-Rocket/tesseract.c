2a0646254073195ec79ba3daeeb26eed83baa2ed
Killian Hourihan <kllnhrhn@gmail.com>
2024-11-03 17:36:39 -0800

    Add line plotting

--- src/main.c
+++ src/main.c
@@ -38,12 +38,34 @@ void plot_point_uv(char buf[BUF_SIZE], float u, float v, float b) {
     plot_point(buf, u * RES_X, v * RES_Y, b);
 }
 
+// Bresenham's line algorithm
+// TODO: Xiaolin Wu's line algorithm
+void plot_line(char buf[BUF_SIZE], int x0, int y0, int x1, int y1) {
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int d = 2 * dy - dx;
+    int y = y0;
+
+    for (int x = x0; x <= x1; x++) {
+        plot_point(buf, x, y, 1.0);
+        if (d > 0) {
+            y++;
+            d -= 2 * dx;
+        }
+        d += 2 * dy;
+    }
+}
+
+void plot_line_uv(char buf[BUF_SIZE], float u0, float v0, float u1, float v1) {
+    plot_line(buf, u0 * RES_X, v0 * RES_Y, u1 * RES_X, v1 * RES_Y);
+}
+
 int main() {
     char buf[BUF_SIZE];
 
     for (;;) {
         memset(buf, 32, BUF_SIZE);
-        plot_point_uv(buf, 0.5, 0.5, 1.0);
+        plot_line_uv(buf, 0.1, 0.2, 0.8, 0.7);
         for (int k = 0; 1761 > k; k++)
             putchar(k % 80 ? buf[k] : 10);
         usleep(15000);

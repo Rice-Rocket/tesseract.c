311a331b1259515b4a44fd346890fbbadfd62664
Killian Hourihan <kllnhrhn@gmail.com>
2024-11-03 17:28:17 -0800

    Add matrix multiplication

--- src/main.c
+++ src/main.c
@@ -6,6 +6,28 @@ const int RES_X = 80;
 const int RES_Y = 22;
 const int BUF_SIZE = RES_X * RES_Y;
 
+typedef struct {
+    float v[4][4];
+} Mat4;
+
+Mat4 matmul(Mat4 *a, Mat4 *b) {
+    Mat4 res;
+
+    for (int x = 0; x < 4; x++) {
+        for (int y = 0; y < 4; y++) {
+            float sum = 0;
+
+            for (int k = 0; k < 4; k++) {
+                sum += a->v[x][k] * b->v[k][y];
+            }
+
+            res.v[x][y] = sum;
+        }
+    }
+
+    return res;
+}
+
 void plot(char buf[BUF_SIZE], float u, float v, float b) {
     int x = u * RES_X;
     int y = v * RES_Y;
